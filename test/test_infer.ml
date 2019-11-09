6d5210e8b671da41f8f72893ab83dcf747238723
thautwarm <twshere@outlook.com>
2019-11-10 02:19:08 +0800

    row polymorphism

--- test/test_infer.ml
+++ test/test_infer.ml
@@ -7,7 +7,7 @@ let _ = let open TC in
   let a = new_tvar() in
   let b = new_tvar() in
   let is_ok = unify (Tuple [a; a]) (Tuple [i32; b]) in
-  let _ = assert (is_ok && prune a = i32 && prune b = i32) in
+  assert (is_ok && prune a = i32 && prune b = i32);
   let rho = new_tvar() in
   let record1 = record ["k", i32] (ExtRef rho) in
   let record2 = record ["a", i32; "k", a] Mono in
