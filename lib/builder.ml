3af456286bedb1324fb21ea032f676c6d24d469f
thautwarm <twshere@outlook.com>
2019-11-24 14:11:01 +0800

    rets dsl: pretty print of solved results

--- lib/builder.ml
+++ lib/builder.ml
@@ -38,6 +38,31 @@ let from_builder : 'r . builder list -> ((module TState) * int option *  int lis
     let m = (module TC: TState) in
     cont (m, e, IntSet.elements !requires)
 
+let is_simple = function
+  | Var _ | Fresh _ | Nom _ | Record _ | Tuple _  -> true
+  | _ -> false
+
+let rec dumpstr = function
+  | App(f, arg) when is_simple arg -> dumpstr f ^ " " ^ dumpstr arg
+  | App(f, arg)  -> dumpstr f ^ " (" ^ dumpstr arg ^ ")"
+  | Arrow(arg, ret) when is_simple arg -> dumpstr arg ^ " -> " ^ dumpstr ret
+  | Arrow(arg, ret) -> "(" ^ dumpstr arg ^ ") -> " ^ dumpstr ret
+  | Forall(ns, t) -> "forall {" ^ String.join " " ns ^ "} " ^ dumpstr t
+  | Tuple(elts) -> "[" ^ String.join ", " (List.map dumpstr elts) ^ "]"
+  | Record rowt -> begin match dumpstr_row rowt with
+    | xs, Some tho -> "{" ^ String.join ", " xs ^ " | " ^ tho ^ "}"
+    | xs, None     -> "{" ^ String.join ", " xs ^ "}"
+    end
+  | Var i -> "'" ^ string_of_int i
+  | Nom i -> "^" ^ string_of_int i
+  | Fresh a -> a
+
+and dumpstr_row : rowt -> (string list * string option) = function
+  | RowCons(n, t, rowt) ->
+    let (xs, o) = dumpstr_row rowt in (n ^ ": " ^ dumpstr t)::xs, o
+  | RowPoly t -> [], Some (dumpstr t)
+  | RowMono -> [], None
+
 let dump out = fun ((module M: TState), r, ns) ->
   let print s = output_string out s in
   let _ = match r with
@@ -51,5 +76,5 @@ let dump out = fun ((module M: TState), r, ns) ->
     in
       print @@ string_of_int i;
       print " ";
-      print @@ show pruned;
+      print @@ dumpstr pruned;
       print "\n"
